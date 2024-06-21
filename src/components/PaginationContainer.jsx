6b7ac95c44bdd712a56ddf72a0d7d2ca6750cf4d
Giovane Pereira <giovane305pereira@gmail.com>
2024-06-20 21:41:02 -0300

    add: paginação

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,9 +1,58 @@
 import React from "react";
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 
 export default function PaginationContainer() {
+  const { info } = useLoaderData();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const { pageCount, page } = info?.pagination || {};
+
+  if (!pageCount || pageCount < 2) return null;
+  const handlePageChange = (pageNumber) => {
+    navigate(`${pathname}?page=${pageNumber}`);
+  };
+
+  const pageButtons = Array.from(
+    { length: pageCount },
+    (_, index) => index + 1
+  );
+
   return (
-    <div>
-      <h1>Pagination</h1>
+    <div className="main-container mt-16 join w-full flex sm:justify-end justify-center flex-wrap sm:flex-nowrap">
+      <button
+        className={`btn btn-md join-item ${
+          page === 1 ? "disabled:opacity-95 hover:cursor-not-allowed" : ""
+        }`}
+        onClick={() => handlePageChange((page - 1 + pageCount) % pageCount)}
+        disabled={page === 1}
+      >
+        Anterior
+      </button>
+
+      {pageButtons.map((pageNumber) => (
+        <button
+          key={pageNumber}
+          onClick={() => handlePageChange(pageNumber)}
+          className={`btn btn-md border-none join-item ${
+            pageNumber === page ? "bg-base-300 border-base-300" : ""
+          }`}
+        >
+          {pageNumber}
+        </button>
+      ))}
+
+      <button
+        className={`btn btn-md join-item ${
+          page === pageCount
+            ? "disabled:opacity-95 hover:cursor-not-allowed"
+            : ""
+        }`}
+        onClick={() => handlePageChange((page + 1) % pageCount)}
+        disabled={page === pageCount}
+      >
+        Próximo
+      </button>
     </div>
   );
 }
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -9,7 +9,6 @@ export default function HomeLayout() {
   return (
     <>
       <Header />
-
       <main>{isPageLoading ? <Loading /> : <Outlet />}</main>
     </>
   );
